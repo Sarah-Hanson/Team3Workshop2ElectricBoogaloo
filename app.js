c40787a8c5c68f575168329f61316e47278d57fc
SinKane <wade.grimm@shaw.ca>
2019-11-28 15:57:42 -0700

    Wade's updates

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/stylesheets')));
+app.use(express.static(path.join(__dirname, 'public/images')));
+app.use(express.static(path.join(__dirname, 'public/scripts')));
 
 console.log("passed init");
 
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,25 @@ router.get('/vacation', function(req, res, next) {
   res.render('vacayPackages.ejs', { title: 'Vacation Packages' });
 });
 
+//Wade's registration page
+router.get('/registration', function(req, res, next) {
+  res.render('registration.ejs', { title: 'Client Registration' });
+});
+
+router.get('/index', function(req, res, next) {
+  res.render('index.ejs', { title: 'Travel Experts' });
+});
+
+router.get('/contactus', function(req, res, next) {
+  res.render('contactus.ejs', { title: 'Contact Us' });
+});
+
+router.get('/vacaypackages', function(req, res, next) {
+  res.render('vacaypackages.ejs', { title: 'Vacation Packages' });
+});
+
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: 'hello' });
+  res.render('index.ejs', { title: 'Travel Experts' });
 });
 
 module.exports = router;
